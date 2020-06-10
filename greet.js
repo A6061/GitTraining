8966de7c8b34f4c429ec3fe679a4cfa819437528
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-06-10 19:23:45 +0200

    c4

--- greet.js
+++ greet.js
@@ -1,14 +1,7 @@
 'use strict';
 
 function greet(name) {
-<<<<<<< HEAD
 	console.log('Greetings!');
 }
 
 greet('Adam');
-=======
-    console.log("My name is " + name + "!");
-  }
-  
-  greet("Adam");
->>>>>>> 9f665e4a934b663524481dbd844a215e45782ace
